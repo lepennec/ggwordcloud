398712418c27cbc333f0849c02320588b7feeeb9
Erwan Le Pennec <lepennec@gmail.com>
2018-10-18 11:55:02 +0200

    Add some tests

--- tests/testthat/test-ggwordcloud.R
+++ tests/testthat/test-ggwordcloud.R
@@ -1,9 +1,27 @@
 context("test-ggwordcloud")
 
-test_that("ggwordcloud works", {
+dat <- mtcars
+dat$name <- row.names(mtcars)
+dat$size <- dat$mpg
+dat$size[1] <- 300
+
+test_that("geom_text_wordcloud does not crash", {
   expect_is({
-    dat <- mtcars
-    dat$name <- row.names(mtcars)
     ggplot(data = dat, aes(label = name)) + geom_text_wordcloud()
+  }, "ggplot")
+})
+
+test_that("geom_text_wordcloud does not crash", {
+  expect_is({
+    ggplot(data = dat, aes(label = name, size = size)) + geom_text_wordcloud()
+  }, "ggplot")
+})
+
+
+context("test-bugfixes")
+
+test_that("No issue with geom_text_wordcloud_area when no size is defined", {
+  expect_is({
+    ggplot(data = dat, aes(label = name)) + geom_text_wordcloud_area()
   }, "ggplot")
 })
