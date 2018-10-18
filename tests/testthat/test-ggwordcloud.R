52b042208c5eb513a4c888df53ee8c30ade82347
Erwan Le Pennec <lepennec@gmail.com>
2018-10-18 16:53:01 +0200

    More tests

--- tests/testthat/test-ggwordcloud.R
+++ tests/testthat/test-ggwordcloud.R
@@ -12,11 +12,21 @@ test_that("geom_text_wordcloud does not crash", {
   }, "ggplot")
 })
 
-test_that("geom_text_wordcloud does not crash", {
+test_that("geom_text_wordcloud_area does not crash", {
   expect_is({
     set.seed(42)
-    print(ggplot(data = dat, aes(label = name, size = size)) + geom_text_wordcloud())
+    print(ggplot(data = dat, aes(label = name, size = size)) + geom_text_wordcloud_area())
   }, "ggplot")
+})
+
+test_that("geom_text_wordcloud works for all shape", {
+  expect_silent({
+    set.seed(42)
+    for (i in 1:8) {
+      print(ggplot(data = dat, aes(label = name)) +
+              geom_text_wordcloud(shape = i))
+    }
+  })
 })
 
 context("test-wrapper")
