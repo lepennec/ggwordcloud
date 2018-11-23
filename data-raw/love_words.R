e22427acb55100b238366f5220683114f3196f51
Enrico Spinielli <enrico.spinielli@eurocontrol.int>
2018-11-23 18:26:29 +0100

    tidy the love words in 2 tables: language statistics and 'love' words. This will allow to manage language statistics independently of the word dictionary and add new word sets, i.e. 'thank you' (which is a nice candidate for word clouds in slides)

--- data-raw/love_words.R
+++ data-raw/love_words.R
@@ -1,4 +1,9 @@
 library(tidyverse)
 love_words <- read_csv("./data-raw/love_words.csv")
-love_words <- amor3 %>% mutate(native_speakers = L1, speakers = L1 + L2) %>% select(-L1, -L2) %>% arrange(desc(speakers))
+lang_stats <- read_csv("data-raw/lang_stats.csv")
+love_words <- love_words %>%
+  left_join(lang_stats) %>%
+  mutate(native_speakers = L1, speakers = L1 + L2) %>%
+  select(-L1, -L2) %>%
+  arrange(desc(speakers))
 love_words_small <- love_words %>% filter(speakers > 50)
