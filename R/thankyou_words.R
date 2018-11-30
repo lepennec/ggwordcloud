ebeb486b23215ab09fa46f6d0b6ff07359b77aa8
Enrico Spinielli <enrico.spinielli@gmail.com>
2018-11-30 17:16:50 +0100

    added thank you dataset

---
+++ R/thankyou_words.R
@@ -0,0 +1,20 @@
+#' 'Thank you' in several languages with number of speakers
+#'
+#' A dataset containing the word 'Thank you' in different languages (133 or 34 for the
+#' small one) as well as the number of native speakers and overall speakers of
+#' those languages.
+#'
+#' @format a data.frame with 133 observations (or 34 for the small one) of 4
+#'   variables
+#'   \describe{
+#'   \item{iso_639_3}{the ISO 639-3 language code}
+#'   \item{word}{the word love in that language}
+#'   \item{native_speakers}{number of native speakers in millions}
+#'   \item{speakers}{number of speakers in millions}
+#'   }
+#'
+#' @source wikipedia
+"thankyou_words"
+
+#' @rdname thankyou_words
+"thankyou_words_small"
--- data-raw/thankyou_words.R
+++ data-raw/thankyou_words.R
@@ -1,10 +1,10 @@
 library(tidyverse)
-love_words <- read_csv("./data-raw/love_words.csv")
+thankyou_words <- read_csv("./data-raw/thankyou_words.csv")
 lang_stats <- read_csv("data-raw/lang_stats.csv")
-love_words <- love_words %>%
+thankyou_words <- thankyou_words %>%
   filter(!is.na(word)) %>%
   left_join(lang_stats) %>%
   mutate(native_speakers = L1, speakers = L1 + L2) %>%
   select(-L1, -L2) %>%
   arrange(desc(speakers))
-love_words_small <- love_words %>% filter(speakers > 50)
+thankyou_words_small <- thankyou_words %>% filter(speakers > 50)
